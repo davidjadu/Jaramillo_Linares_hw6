027101cf9b24132232c13ff1a5d405bd485026ba
davidjadu <davidjadu@hotmail.com>
2014-10-26 22:59:23 -0500

    runge-kutta for problem 2(leapforg yields to undesired results, runge-kutta yields to lines)

--- Trayectoria en campo magnético/plot_trayectoria.py
+++ Trayectoria en campo magnético/plot_trayectoria.py
@@ -10,18 +10,19 @@ data = np.loadtxt(filename);
 fig = plt.figure()
 ax=plt.axes()
 ax.plot(data[:,1],data[:,2])
-ax.set_xlabel('x')
-ax.set_ylabel('y')
+ax.set_xlabel('$x[m]$')
+ax.set_ylabel('$y[m]$')
 plt.savefig(filename[:-4] +'.pdf', format = 'pdf', transparent = True)
 plt.close
 
 
 fig = plt.figure()
-bx = fig.add_subplot(111, projection = '3d')
-bx.plot_wireframe(data[:,1],data[:,2], data[:,3])
-bx.set_xlabel('x')
-bx.set_ylabel('y')
-bx.set_zlabel('z')
+ax = fig.add_subplot(111, projection = '3d')
+ax.plot(data[:,1], data[:,2], data[:,3])
+ax.set_xlabel('$x[m]$')
+ax.set_ylabel('$y[m]$')
+ax.set_zlabel('$z[m]$')
 plt.savefig(filename[:-4] +'3d.pdf', format = 'pdf', transparent = True)
+plt.show()
 plt.close
 
--- Trayectoria en campo magnético/runge_kutta.c
+++ Trayectoria en campo magnético/runge_kutta.c
@@ -10,6 +10,7 @@ double m = 0.938272; // GeV/c^2
 double vx_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
 double vy_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
 double vz_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
+void make_step(double *x,double *y,double  *z,double *vx,double *vy,double *vz,double *x_old,double  *y_old,double *z_old,double *vx_old,double *vy_old,double  *vz_old,double *t,double  dt, double v0);
 
 int main(int argc, char **argv){
   int points =10000;
@@ -23,7 +24,7 @@ int main(int argc, char **argv){
   double dt = 100.0/points;
 
   char filename[50];
-  sprintf(filename, "trayectoria_%f_%f.dat",KE,alpha);
+  sprintf(filename, "trayectoria_%f_%f_runge_kutta.dat",KE,alpha);
   FILE *out= fopen(filename,"w");
   fprintf(out,"%f %f %f %f\n",t,x,y,z);
   t=t+dt;
@@ -42,16 +43,8 @@ int main(int argc, char **argv){
 
   //Leap frog
   for(i=1; i<points; i++){
-    vx=vx_old2 + 2*dt*vx_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    vy=vy_old2 + 2*dt*vy_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    vz=vz_old2 + 2*dt*vz_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    x = x_old2 + 2*dt*vx;
-    y = y_old2 + 2*dt*vy;
-    z = z_old2 + 2*dt*vz;
-    t=t+dt;
+    make_step(&x,&y,&z,&vx,&vy,&vz,&x_old,&y_old,&z_old,&vx_old,&vy_old,&vz_old,&t, dt, v0);
     fprintf(out,"%f %f %f %f\n",t,x,y,z);
-    x_old2=x_old,y_old2=y_old,z_old2=z_old,vx_old2=vx_old,vy_old2=vy_old,vz_old2=vz_old;
-    x_old=x,y_old=y,z_old=z , vx_old=vx ,vy_old=vy,vz_old=vz;
   }
   fclose(out);
   return 0;
@@ -68,4 +61,63 @@ double vy_prime(double x,double y,double z,double vx,double vy,double vz,double 
 
 double vz_prime(double x,double y,double z,double vx,double vy,double vz,double v0){
   return pow(Rt,3)*sqrt(1+pow(v0/c,2))*qB0_m*(-3*vx*y*z+3*x*z*vy)/pow(pow(x,2)+pow(y,2)+pow(z,2),5.0/2.0);
+}
+
+void make_step(double *x,double *y,double  *z,double *vx,double *vy,double *vz,double *x_old,double  *y_old,double *z_old,double *vx_old,double *vy_old,double  *vz_old,double *t,double  dt, double v0){
+  
+  double ax1 = vx_prime(*x,*y,*z,*vx,*vy,*vz,v0);
+  double ay1 = vy_prime(*x,*y,*z,*vx,*vy,*vz,v0);
+  double az1 = vz_prime(*x,*y,*z,*vx,*vy,*vz,v0);
+  
+  double vx1 = *vx_old+dt/2.0*ax1;
+  double vy1 = *vy_old+dt/2.0*ay1;
+  double vz1 = *vz_old+dt/2.0*az1;
+  double x1 = *x_old+dt/2.0*vx1;
+  double y1 = *y_old+dt/2.0*vy1;
+  double z1 = *z_old+dt/2.0*vz1;
+  double ax2 = vx_prime(x1,y1,z1,vx1,vy1,vz1,v0);
+  double ay2 = vy_prime(x1,y1,z1,vx1,vy1,vz1,v0); 
+  double az2 = vz_prime(x1,y1,z1,vx1,vy1,vz1,v0);
+
+  double vx2 = *vx_old+dt/2.0*ax2;
+  double vy2 = *vy_old+dt/2.0*ay2;
+  double vz2 = *vz_old+dt/2.0*az2;
+  double x2 = *x_old+dt/2.0*vx2;
+  double y2 = *y_old+dt/2.0*vy2;
+  double z2 = *z_old+dt/2.0*vz2;
+  double ax3 = vx_prime(x2,y2,z2,vx2,vy2,vz2,v0);
+  double ay3 = vy_prime(x2,y2,z2,vx2,vy2,vz2,v0);
+  double az3 = vz_prime(x2,y2,z2,vx2,vy2,vz2,v0);
+
+  double vx3 = *vx_old+dt*ax3;
+  double vy3 = *vy_old+dt*ay3;
+  double vz3 = *vz_old+dt*az3;
+  double x3 = *x_old+dt*vx3;
+  double y3 = *y_old+dt*vy3;
+  double z3 = *z_old+dt*vz3;
+  double ax4 = vx_prime(x3,y3,z3,vx3,vy3,vz3,v0);
+  double ay4 = vy_prime(x3,y3,z3,vx3,vy3,vz3,v0);
+  double az4 = vz_prime(x3,y3,z3,vx3,vy3,vz3,v0);
+
+  double ax=1.0/6.0*(ax1+2.0*ax2+2.0*ax3+ax4);
+  double ay=1.0/6.0*(ay1+2.0*ay2+2.0*ay3+ay4);
+  double az=1.0/6.0*(az1+2.0*az2+2.0*az3+az4);
+
+  double new_vx = 1.0/6.0* (*vx + 2.0*vx1+ 2.0*vx2 +vx3);
+  double new_vy = 1.0/6.0* (*vx + 2.0*vx1+ 2.0*vx2 +vx3);
+  double new_vz = 1.0/6.0* (*vx + 2.0*vx1+ 2.0*vx2 +vx3);
+
+  *x_old = *x;
+  *y_old = *y;
+  *z_old = *z;
+  *vx_old = *vx;
+  *vy_old = *vz;
+  *vz_old = *vz;
+  *t=*t+dt;
+  *y= *y + new_vy*dt;
+  *x= *x + new_vx*dt;
+  *z=*z+new_vz*dt;
+  *vx=*vx+ax*dt;
+  *vy=*vy+ay*dt;
+  *vz=*vz+az*dt;
 }
