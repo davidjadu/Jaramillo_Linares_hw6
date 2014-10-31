be059b9ffbba0f8a43e1178e31f57ddb8d7dd45d
davidjadu <davidjadu@hotmail.com>
2014-10-30 21:37:33 -0500

    pproblem 2 done, there is just the makefile missing

--- Punto2/particle_in_field.c
+++ Punto2/particle_in_field.c
@@ -2,70 +2,160 @@
 #include<stdlib.h>
 #include<math.h>
 
-double qB0_m = 2874; // qB0/m [1/s]
-double Rt = 6378.1E3; // m
-double c = 3E8; // m/s^2
-double m = 0.938272; // GeV/c^2
+float qB0_m = 2874; // qB0/m [1/s]
+float c = 47; // m/s^2
+float m = 938.272; // MeV/c^2
 
-double vx_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
-double vy_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
-double vz_prime(double x,double y,double z,double vx,double vy,double vz,double v0);
+void make_step(float *r, float *v, float *r_old, float *v_old, float *t, float dt);
+float *scalar_mult(float lambda, float *a);
+float *v_prime(float *r, float *v);
+float *qB_mvector(float *r);
+float norm(float *a);
+float *cross_product(float *a, float *b);
+float *copy(float *a);
+float *add_vector(float *a, float *b);
 
 int main(int argc, char **argv){
-  int points =10000;
-  double KE = atof(argv[1]); // KE is GeV
-  double alpha = atof(argv[2]);
-  double x=2*Rt,y=0,z=0;
-  double x_old, y_old,z_old,vx_old,vy_old,vz_old,x_old2, y_old2,z_old2,vx_old2,vy_old2,vz_old2;
-  double v0 = c*sqrt(1- 1/pow( KE/(2*m) + 1 ,2) ); // Note is constant as the only force is due to the magnetic field. Hence gamma is constant
-  double vx = 0, vy = v0*sin(alpha), vz = v0*cos(alpha);
-  double t=0;
-  double dt = 100.0/points;
-
+  float KE = atof(argv[1]); // KE is MeV
+  int points = 1E6;
+  float alpha_deg = atof(argv[2]);
+  float alpha = alpha_deg*M_PI/180;
+  float *r = malloc(3*sizeof(float));
+  r[0]=2, r[1]=0, r[2]=0;
+  float *r_old = malloc(3*sizeof(float));
+  float v0 = c*sqrt(1- 1/pow( KE/(2*m) + 1 ,2) );
+  float *v =  malloc(3*sizeof(float));
+  v[0] = 0, v[1] = v0*sin(alpha), v[2] = v0*cos(alpha);
+  float *v_old = malloc(3*sizeof(float));
+  float t=0;
+  float dt = 100.0/points;
   char filename[50];
-  sprintf(filename, "trayectoria_%f_%f.dat",KE,alpha);
+  sprintf(filename, "trayectoria_%.1f_%.1f_runge_kutta.dat",KE,alpha_deg);
   FILE *out= fopen(filename,"w");
-  fprintf(out,"%f %f %f %f\n",t,x,y,z);
+  fprintf(out,"%f %f %f %f\n",t,r[0],r[1],r[2]);
   t=t+dt;
-  x_old=x,y_old=y,z_old=z,vx_old=vx,vy_old=vy,vz_old=vz;
-  vx = vx_old + dt*vx_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-  vy = vy_old + dt*vy_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-  vz = vz_old + dt*vz_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-  x=x_old+dt*vx/Rt;
-  y=y_old+dt*vy/Rt;
-  z=z_old+dt*vz/Rt;
-  fprintf(out,"%f %f %f %f\n",t,x,y,z);
-  x_old2=x_old,y_old2=y_old,z_old2=z_old,vx_old2=vx_old,vy_old2=vy_old,vz_old2=vz_old;
-  x_old=x,y_old=y,z_old=z,vx_old=vx,vy_old=vy,vz_old=vz;
+  r_old = copy(r);
+  r = add_vector(scalar_mult(dt,v),r_old);
+  v_old = copy(v);
+  float *a = v_prime(r_old,v_old);
+  v = add_vector(scalar_mult(dt,a), v_old);
+  fprintf(out,"%f %f %f %f\n",t,r[0],r[1],r[2]);
 
   int i;
 
-  //Leap frog
   for(i=1; i<points; i++){
-    vx=vx_old2 + 2*dt*vx_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    vy=vy_old2 + 2*dt*vy_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    vz=vz_old2 + 2*dt*vz_prime(x_old,y_old,z_old,vx_old,vy_old,vz_old,v0);
-    x = x_old2 + 2*dt*vx;
-    y = y_old2 + 2*dt*vy;
-    z = z_old2 + 2*dt*vz;
+
+ float *a1 = v_prime(r_old,v_old);
+
+    float *v1 = add_vector(scalar_mult(dt/2.0,a1),v_old);
+    float *r1 = add_vector(scalar_mult(dt/2.0,v),r_old);
+    float *a2 = v_prime(r1,v1);
+
+    float *v2 = add_vector(scalar_mult(dt/2.0,a2),v_old);
+    float *r2 = add_vector(scalar_mult(dt/2.0,v1),r_old);
+    float *a3 = v_prime(r2,v2);
+
+    float *v3 = add_vector(scalar_mult(dt,a3),v_old);
+    float *r3 = add_vector(scalar_mult(dt,v2),r_old);
+    float *a4 = v_prime(r3,v3);
+
+    float *a = scalar_mult(1/6.0, add_vector(scalar_mult(2.0,add_vector(a2,a3)),add_vector(a1, a4)));
+    float *newv = scalar_mult(1/6.0, add_vector(scalar_mult(2.0,add_vector(v1,v2)),add_vector(v, v3)));
+
     t=t+dt;
-    fprintf(out,"%f %f %f %f\n",t,x,y,z);
-    x_old2=x_old,y_old2=y_old,z_old2=z_old,vx_old2=vx_old,vy_old2=vy_old,vz_old2=vz_old;
-    x_old=x,y_old=y,z_old=z , vx_old=vx ,vy_old=vy,vz_old=vz;
+    r = add_vector(scalar_mult(dt, newv), r_old);
+    v = add_vector(scalar_mult(dt, a), v_old);
+    r_old = copy(r);
+    v_old = copy(v);
+
+    fprintf(out,"%f %f %f %f\n",t,r[0],r[1],r[2]);
   }
   fclose(out);
   return 0;
 
 }
 
-double vx_prime(double x,double y,double z,double vx,double vy,double vz,double v0){
-  return pow(Rt,3)*sqrt(1+pow(v0/c,2))*qB0_m*(-vy*(2*pow(z,2)-pow(x,2)-pow(y,2))+3*y*z*vz)/pow(pow(x,2)+pow(y,2)+pow(z,2),5.0/2.0);
+float *v_prime(float *r, float *v){
+  float *a= scalar_mult(sqrt(1-pow(norm(v)/c,2)), cross_product(v,qB_mvector(r)));
+  return a;
 }
 
-double vy_prime(double x,double y,double z,double vx,double vy,double vz,double v0){
-  return pow(Rt,3)*sqrt(1+pow(v0/c,2))*qB0_m*(vx*(2*pow(z,2)-pow(x,2)-pow(y,2))-3*x*z*vz)/pow(pow(x,2)+pow(y,2)+pow(z,2),5.0/2.0);
+//copy a vector
+float *copy(float *a){
+  float *c = malloc(3*sizeof(float));
+  int i; 
+  for (i=0; i<3; i++)
+    c[i]=a[i];
+  return c;
 }
 
-double vz_prime(double x,double y,double z,double vx,double vy,double vz,double v0){
-  return pow(Rt,3)*sqrt(1+pow(v0/c,2))*qB0_m*(-3*vx*y*z+3*x*z*vy)/pow(pow(x,2)+pow(y,2)+pow(z,2),5.0/2.0);
+// q*B/(mr^5) in mks
+float *qB_mvector(float *r){
+  float *B=malloc(3*sizeof(float));
+  B[0]=-qB0_m/pow(norm(r),5)*3*r[0]*r[2];
+  B[1]=-qB0_m/pow(norm(r),5)*3*r[1]*r[2];
+  B[2]=-qB0_m/pow(norm(r),5)*(2*pow(r[2],2)-pow(r[1],2)-pow(r[0],2));
+  return B;
+}
+
+// Vector norm
+float norm(float *a){
+  float norm_squared = 0;
+  int i;
+  for(i=0; i<3; i++)
+    norm_squared+=pow(a[i],2);
+  return sqrt(norm_squared);
+}
+
+// Vector cross product
+float *cross_product(float *a, float *b){
+  float *c = malloc(3*sizeof(float));
+  c[0]=a[1]*b[2]-a[2]*b[1];
+  c[1]=a[2]*b[0]-a[0]*b[2];
+  c[2]=a[0]*b[1]-a[1]*b[0];
+  return c;
+}
+
+// Vector addition
+float *add_vector(float *a, float *b){
+  float *c = malloc(3*sizeof(float));
+  int i;
+  for(i=0; i<3; i++)
+    c[i]=a[i]+b[i];
+  return c;
+}
+
+//Scalar times vector multiplication
+float *scalar_mult(float lambda, float *a){
+  float *new = malloc(3*sizeof(float));
+  int i;
+  for(i=0; i<3; i++)
+    new[i]=lambda*a[i];
+  return new;
+}
+
+
+void make_step(float *r, float *v, float *r_old, float *v_old, float *t, float dt){
+    float *a1 = v_prime(r_old,v_old);
+
+    float *v1 = add_vector(scalar_mult(dt/2.0,a1),v_old);
+    float *r1 = add_vector(scalar_mult(dt/2.0,v),r_old);
+    float *a2 = v_prime(r1,v1);
+
+    float *v2 = add_vector(scalar_mult(dt/2.0,a2),v_old);
+    float *r2 = add_vector(scalar_mult(dt/2.0,v1),r_old);
+    float *a3 = v_prime(r2,v2);
+
+    float *v3 = add_vector(scalar_mult(dt,a3),v_old);
+    float *r3 = add_vector(scalar_mult(dt,v2),r_old);
+    float *a4 = v_prime(r3,v3);
+
+    float *a = scalar_mult(1/6.0, add_vector(scalar_mult(2.0,add_vector(a2,a3)),add_vector(a1, a4)));
+    float *newv = scalar_mult(1/6.0, add_vector(scalar_mult(2.0,add_vector(v1,v2)),add_vector(v, v3)));
+
+    *t=*t+dt;
+    r = add_vector(scalar_mult(dt, newv), r_old);
+    v = add_vector(scalar_mult(dt, a), v_old);
+    r_old = copy(r);
+    v_old = copy(v);
 }
--- Punto2/plot_trayectoria.py
+++ Punto2/plot_trayectoria.py
@@ -9,20 +9,27 @@ data = np.loadtxt(filename);
 
 fig = plt.figure()
 ax=plt.axes()
-ax.plot(data[:,1],data[:,2])
-ax.set_xlabel('$x[m]$')
-ax.set_ylabel('$y[m]$')
+ax.plot(data[:,1],data[:,2], color ='r')
+ax.set_xlabel('$x[R_t]$')
+ax.set_ylabel('$y[R_t]$')
 plt.savefig(filename[:-4] +'.pdf', format = 'pdf', transparent = True)
 plt.close
 
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
-ax.plot(data[:,1], data[:,2], data[:,3])
-ax.set_xlabel('$x[m]$')
-ax.set_ylabel('$y[m]$')
-ax.set_zlabel('$z[m]$')
+theta = np.linspace(0,2*np.pi,100);
+phi = np.linspace(0,np.pi, 100);
+x = np.outer(np.cos(theta), np.sin(phi))
+y = np.outer(np.sin(theta), np.sin(phi))
+z = np.outer(np.ones(100), np.cos(phi))
+ax.plot_surface(x,y,z, rstride=3,cstride=3, color ='b')
+ax.plot(data[:,1], data[:,2], data[:,3], color = 'r')
+ax.set_xlabel('$x[R_t]$')
+ax.set_ylabel('$y[R_t]$')
+ax.set_zlabel('$z[R_t]$')
+ax.set_xlim(-2.0,2.0)
+ax.set_ylim(-2.0,2.0)
+ax.set_zlim(-2.0,2.0)
 plt.savefig(filename[:-4] +'3d.pdf', format = 'pdf', transparent = True)
-plt.show()
 plt.close
 
